36b1d7d7e8e486dc8488130516834fb6dee74a68
Evan Tellep <evantellep@gmail.com>
2020-04-06 16:02:09 -0400

    Cubes now respond to noise.

--- MarchingCubes.gd
+++ MarchingCubes.gd
@@ -7,24 +7,42 @@ var dictionaryCheckQuadVerticies = {}
 
 const CUBE_SIZE = 1
 
+var noise = OpenSimplexNoise.new()
+
+var terrain = {}
+
+var GRID_SCALE = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	noise.seed = 10
+	noise.octaves = 4
+	noise.period = 20
+	noise.persistence = 0.8
+	
+	for x in range(10):
+		for y in range(10):
+			if terrain.has((str(x) + ',' + str(y))) == false:
+				terrain[(str(x) + ',' + str(y))] = noise.get_noise_2d(x, y) * GRID_SCALE
+	
+	# print(terrain)
+	
+	var offset = 2
+	var xOffset
+	var yOffset
+	var zOffset
 	# Just some fun code for now that creates a grid of blocks at different dimensions.
-	for x in range(6):
-		if x == 0:
-			makeCube(1, 1, 1)
-		else:
-			makeCube(x, 1, 1)
-		for y in range(3):
-			if y == 0:
-				makeCube(1, 1, 1)
-			else:
-				makeCube(x, y, 1)
-			for z in range(3):
-				if z == 0:
-					makeCube(1, 1, 1)
-				else:
-					makeCube(x, y, z)
+	for x in range(100):
+		xOffset = offset * x
+
+		for y in range(1):
+			yOffset = offset * y
+
+			for z in range(100):
+				zOffset = offset * z
+				
+				print(noise.get_noise_2d(x, z))
+				makeCube(xOffset, floor(noise.get_noise_2d(x, z) * 10), zOffset)
 
 # Make a cube and specify offsets to move the cubes around.
 func makeCube(offsetX, offsetY, offsetZ):
@@ -37,15 +55,15 @@ func makeCube(offsetX, offsetY, offsetZ):
 	
 	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
 	
-	var vertNorthTopLeft     = Vector3( CUBE_SIZE * offsetX,  CUBE_SIZE * offsetY,  CUBE_SIZE * offsetZ)
-	var vertNorthBottomLeft  = Vector3( CUBE_SIZE * offsetX,  CUBE_SIZE * offsetY, -CUBE_SIZE * offsetZ)
-	var vertNorthTopRight    = Vector3(-CUBE_SIZE * offsetX,  CUBE_SIZE * offsetY,  CUBE_SIZE * offsetZ)
-	var vertNorthBottomRight = Vector3(-CUBE_SIZE * offsetX,  CUBE_SIZE * offsetY, -CUBE_SIZE * offsetZ)
+	var vertNorthTopLeft     = Vector3( CUBE_SIZE + offsetX,  CUBE_SIZE + offsetY,  CUBE_SIZE + offsetZ)
+	var vertNorthBottomLeft  = Vector3( CUBE_SIZE + offsetX,  CUBE_SIZE + offsetY, -CUBE_SIZE + offsetZ)
+	var vertNorthTopRight    = Vector3(-CUBE_SIZE + offsetX,  CUBE_SIZE + offsetY,  CUBE_SIZE + offsetZ)
+	var vertNorthBottomRight = Vector3(-CUBE_SIZE + offsetX,  CUBE_SIZE + offsetY, -CUBE_SIZE + offsetZ)
 	
-	var vertSouthTopRight    = Vector3(-CUBE_SIZE * offsetX, -CUBE_SIZE * offsetY,  CUBE_SIZE * offsetZ)
-	var vertSouthTopLeft     = Vector3( CUBE_SIZE * offsetX, -CUBE_SIZE * offsetY,  CUBE_SIZE * offsetZ)
-	var vertSouthBottomLeft  = Vector3( CUBE_SIZE * offsetX, -CUBE_SIZE * offsetY, -CUBE_SIZE * offsetZ)
-	var vertSouthBottomRight = Vector3(-CUBE_SIZE * offsetX, -CUBE_SIZE * offsetY, -CUBE_SIZE * offsetZ)
+	var vertSouthTopRight    = Vector3(-CUBE_SIZE + offsetX, -CUBE_SIZE + offsetY,  CUBE_SIZE + offsetZ)
+	var vertSouthTopLeft     = Vector3( CUBE_SIZE + offsetX, -CUBE_SIZE + offsetY,  CUBE_SIZE + offsetZ)
+	var vertSouthBottomLeft  = Vector3( CUBE_SIZE + offsetX, -CUBE_SIZE + offsetY, -CUBE_SIZE + offsetZ)
+	var vertSouthBottomRight = Vector3(-CUBE_SIZE + offsetX, -CUBE_SIZE + offsetY, -CUBE_SIZE + offsetZ)
 	
 	# Make the six quads for needed to make a box!
 	# ============================================
@@ -73,6 +91,7 @@ func makeCube(offsetX, offsetY, offsetZ):
 	# resultMesh.surface_get_arrays() <- Could be good to delete duplicate faces in a larger mesh.
 	var cube = MeshInstance.new()
 	cube.mesh = resultMesh
+	cube.create_trimesh_collision()
 	self.add_child(cube)
 
 # The points must be added in clockwise or counter-clockwise or else this function will not work!
